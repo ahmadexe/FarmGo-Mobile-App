3c81dc72d39e715fddf4fed8ae029398c87c714a
Ahmad <muahmad710@gmail.com>
2023-01-04 11:32:43 +0500

    conf

--- lib/static/api constants/keys.dart
+++ lib/static/api constants/keys.dart
@@ -1,2 +1,3 @@
 const String newsApiKey = '1138a2cde05a468d8db41f047d0cbf16';
 const String apiKey = 'AIzaSyAaZyLRjCADuV1QH5Hw5ZN8UCHn3KYPQWo';
+const String weatherApiKey = 'f0bd4fd099b1d8eb0659d97921ccb2b6';
